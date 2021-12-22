1b4bfcfbbb8424e30df89d9065a7a711ba2b3900
Lce <luciancerbu87@gmail.com>
2021-12-23 01:17:30 +0200

    Initial Project Setup - No Storyboard
    Removing the storyboard
    SceneDelegate (New in iOS 13)
    App Icon
    Light/Dark Mode assets
    SF Symbols

---
+++ gitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitHubFollowers
+//
+//  Created by Lucian Cerbu on 23.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.systemBlue
+    }
+
+
+}
+
