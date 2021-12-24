fe0a3cca76bb7f652c43def6417d58233dff998a
Lce <luciancerbu87@gmail.com>
2021-12-24 20:20:44 +0200

    Create Network Call - Get Followers
    Pros and Cons of Singletons
    Asynchronous Network Calls
    Closures
    HTTP Status Codes
    Codable
    Error Handling (Do, Try, Catch)
    
    Additional infos
    
    - HTTP Status Codes:
            https://en.wikipedia.org/wiki/List_of_HTTP_status_codes

--- gitHubFollowers/Screens/FollowerListVC.swift
+++ gitHubFollowers/Screens/FollowerListVC.swift
@@ -16,6 +16,16 @@ class FollowerListVC: UIViewController {
         
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad stuff happend", message: errorMessage!, buttonTitle: "OK")
+                return
+            }
+
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
