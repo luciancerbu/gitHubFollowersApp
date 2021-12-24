b7ccfd2bc62180faae190fb443e8e779770cfedd
Lce <luciancerbu87@gmail.com>
2021-12-24 21:57:02 +0200

    Refactor Error Messages
     - Why write our own errors?
     - A quick bug fix on our GFButton
     - error.localizedDescription
     - Enum Raw Values
    
    Enums Documentation:
    https://docs.swift.org/swift-book/LanguageGuide/Enumerations.html

---
+++ gitHubFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  gitHubFollowers
+//
+//  Created by Lucian Cerbu on 24.12.2021.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from server.Please try again."
+    case invalidData = "Data received from server was invalid.Please try again."
+}
