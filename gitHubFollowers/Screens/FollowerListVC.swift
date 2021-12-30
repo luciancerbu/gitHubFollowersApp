baf4d5c742b1dfb37b7d7eeaf1fa44458ee65ea8
Lce <luciancerbu87@gmail.com>
2021-12-31 01:33:48 +0200

    [4.4, 4.5]Diffable Data Source and ARC, Memory Leaks, Capture Lists [weak self]
    UICollectionViewDiffableDataSource
    Hashable & Hash Functions
    
    ARC (Automatic Reference Counting)
    Memory Leaks
    Capture Lists [weak self]
    Refactoring our FlowLayout
    
    Automatic Reference Counting Documentation:
    
    https://docs.swift.org/swift-book/LanguageGuide/AutomaticReferenceCounting.html

--- gitHubFollowers/Screens/FollowerListVC.swift
+++ gitHubFollowers/Screens/FollowerListVC.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
     var username: String!
+    var followers: [Follower] = []
+    
     var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,8 @@ class FollowerListVC: UIViewController {
         configureViewController()
         configureCollectionView()
         getFollowers()
+        
+        configureDataSource()
     }
     
     
@@ -35,11 +44,13 @@ class FollowerListVC: UIViewController {
     
     
     func getFollowers() {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+            guard let self = self else { return }
             
             switch result {
             case .success(let followers):
-                print(followers)
+                self.followers = followers
+                self.updateData()
                 
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad stuff happend", message: error.rawValue, buttonTitle: "OK")
@@ -48,28 +59,31 @@ class FollowerListVC: UIViewController {
     }
     
     
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth: CGFloat = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth: CGFloat = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        
-        return flowLayout
-    }
-    
-    
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         
-        collectionView.backgroundColor = .systemPink
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseId)
     }
 
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseId, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            
+            return cell
+        })
+    }
+    
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+    }
 }
---
+++ gitHubFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,24 @@
+//
+//  UIHelper.swift
+//  gitHubFollowers
+//
+//  Created by Lucian Cerbu on 31.12.2021.
+//
+
+import UIKit
+
+struct UIHelper {
+    static func createThreeColumnFlowLayout(in view:UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth: CGFloat = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth: CGFloat = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+}
