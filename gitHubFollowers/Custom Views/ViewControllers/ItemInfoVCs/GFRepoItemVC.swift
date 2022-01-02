bb597d94135b5becc5a278236fc0167958c39eb9
Lce <luciancerbu87@gmail.com>
2022-01-02 23:09:11 +0200

    [5.6, 5.7, 5.8]Item Info View, ItemInfoVC - Superclass, ItemInfoVC - Subclasses
    [5.6]Item Info View
    Enums
    Switch Statements
    
    [5.7]Superclasses
    UIStackView
    
    UIStackView Documentation:
    https://developer.apple.com/documentation/uikit/uistackview
    
    [5.8]Subclassing & Inheritance

---
+++ gitHubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,24 @@
+//
+//  GFRepoItemVC.swift
+//  gitHubFollowers
+//
+//  Created by Lucian Cerbu on 02.01.2022.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+        
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, with: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, with: user.publicGists)
+        actionButton.set(backgroudColor: .systemPurple, title: "Github Profile")
+    }
+}
