cbe572dcabdfbb9c140c59be097eddcdaf0c53bb
Lce <luciancerbu87@gmail.com>
2022-01-02 21:31:01 +0200

    [5.4] Child View Controller - UserInfoHeaderVC
    Child ViewControllers
    ViewControllers vs. UIViews
    SecondaryTitleLabel
    SFSymbols
    Constants File

---
+++ gitHubFollowers/Custom Views/Labels/GFSecondaryLabel.swift
@@ -0,0 +1,37 @@
+//
+//  GFSecondaryLabel.swift
+//  gitHubFollowers
+//
+//  Created by Lucian Cerbu on 02.01.2022.
+//
+
+import UIKit
+
+class GFSecondaryLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        
+        configure()
+    }
+    
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ gitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  gitHubFollowers
+//
+//  Created by Lucian Cerbu on 02.01.2022.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
