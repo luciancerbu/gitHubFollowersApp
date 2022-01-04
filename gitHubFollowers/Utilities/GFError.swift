6728d4316c5c7828f800006127d81409a0ea1e44
Lce <luciancerbu87@gmail.com>
2022-01-04 11:05:34 +0200

    [6.1 6.2 6.3]Persistence Overview & Add Button, Persistence Manager
    [6.1]Persistence Overview
    UIBarButtonItem
    [6.2]
    Persistence Manger
    Result Type
    Encoding/Decoding
    [6.3]
    Hooking up our PersistenceManager

--- gitHubFollowers/Utilities/GFError.swift
+++ gitHubFollowers/Utilities/GFError.swift
@@ -12,4 +12,6 @@ enum GFError: String, Error {
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidResponse = "Invalid response from server.Please try again."
     case invalidData = "Data received from server was invalid.Please try again."
+    case unableToFavorite = "There was a error saving this user.Please try again."
+    case alreadyInFavorites = "You already added this user 🤔. You must REALLY liked them!"
 }
